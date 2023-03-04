67efb66277b6b4b68517d122c5272ffd427e0577
Richard Smythe <richsmythe@gmail.com>
2023-03-04 14:26:44 +0000

    update

--- ObjectMetaDataTagging/Program.cs
+++ ObjectMetaDataTagging/Program.cs
@@ -5,19 +5,38 @@ namespace ObjectMetaDataTagging
 {
     // A flexible mechanism for associating metadata with object instances.
 
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+
+
     public class Program { 
         static void Main(string[] args)
         {
             // To do: Querying tags, event driven tagging, allowing multiple tags per Set, 
 
             string s = "I am a string object";      
-
             s.SetTag("foo");
             s.SetTag("bar");
             s.SetTag(12);
 
             var allTags = s.GetAllTags();
             foreach (var tag in allTags)
+            {
+                Console.WriteLine(tag);
+            }
+
+            Person p = new Person { Name = "John", Age = 30 };
+            p.SetTag("Suspicious");
+
+            foreach (var tag in p.GetAllTags())
             {
                 Console.WriteLine(tag);
             }
