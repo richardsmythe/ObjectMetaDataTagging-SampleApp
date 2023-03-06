bee6a3d2c131ddca4f9a9ab1e3c711a005994e42
Richard Smythe <richsmythe@gmail.com>
2023-03-06 17:25:50 +0000

    update

--- ObjectMetaDataTagging/Program.cs
+++ ObjectMetaDataTagging/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using System.Security.Cryptography;
+using static ObjectMetaDataTagging.ObjectTaggingExtensions;
 
 namespace ObjectMetaDataTagging
 {
@@ -17,12 +18,13 @@ namespace ObjectMetaDataTagging
     }
 
 
-    public class Program { 
+    public class Program
+    {
         static void Main(string[] args)
         {
             // To do: Querying tags, event driven tagging, allowing multiple tags per Set, 
 
-            string s = "I am a string object";      
+            string s = "I am a string object";
             s.SetTag("foo");
             s.SetTag("bar");
             s.SetTag(12);
@@ -48,6 +50,18 @@ namespace ObjectMetaDataTagging
         private static Dictionary<WeakReference, List<object>> data
            = new Dictionary<WeakReference, List<object>>();
 
+        private static readonly TaggingEventManager _eventManager = new TaggingEventManager();
+        public static event EventHandler<TagAddedEventArgs> TagAdded
+        {
+            add => _eventManager.TagAdded += value;
+            remove => _eventManager.TagAdded -= value;
+        }
+
+        public static void AddTagAddedHandler(EventHandler<TagAddedEventArgs> handler)
+        {
+            TagAdded += handler;
+        }
+
         public static T? GetTag<T>(this object o, int tagIndex)
         {
             var key = data.Keys.FirstOrDefault(k => k.IsAlive && k.Target == o);
@@ -67,10 +81,12 @@ namespace ObjectMetaDataTagging
             if (key != null)
             {
                 data[key].Add(tag);
+                _eventManager.RaiseTagAdded(new TagAddedEventArgs(o, tag));
             }
             else
             {
                 data.Add(new WeakReference(o), new List<object> { tag });
+                _eventManager.RaiseTagAdded(new TagAddedEventArgs(o, tag));
             }
         }
 
@@ -102,7 +118,7 @@ namespace ObjectMetaDataTagging
                     }
                     else
                     {
-                        tags.Add(new KeyValuePair<string, object>(o.GetType().ToString(), 
+                        tags.Add(new KeyValuePair<string, object>(o.GetType().ToString(),
                             new KeyValuePair<string, object>(value.GetType().ToString(), value)));
                     }
                 }
@@ -110,8 +126,28 @@ namespace ObjectMetaDataTagging
             return tags;
         }
 
+        public class TagAddedEventArgs : EventArgs
+        {
+            public object Object { get; set; }
+            public object Tag { get; set; }
 
+            public TagAddedEventArgs(object obj, object tag)
+            {
+                Object = obj;
+                Tag = tag;
+            }
+        }
+
+        private class TaggingEventManager
+        {
+            public event EventHandler<TagAddedEventArgs> TagAdded;
+
+            public void RaiseTagAdded(TagAddedEventArgs e)
+            {
+                TagAdded?.Invoke(this, e);
+                Console.WriteLine($"'{e.Tag}' tag was created and added to object: '{e.Object.GetType()}'.");
+            }
+        }
     }
-
 
 }
