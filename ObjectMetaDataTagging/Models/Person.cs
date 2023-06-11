d162b505b54c16e1acf0bddaba8bfb59f89144e1
Richard Smythe <richsmythe@gmail.com>
2023-06-11 10:34:43 +0100

    update

---
+++ ObjectMetaDataTagging/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace ObjectMetaDataTagging.Models
+{
+    // A flexible mechanism for associating metadata with object instances.
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+
+}
--- ObjectMetaDataTagging/Program.cs
+++ ObjectMetaDataTagging/Program.cs
@@ -1,23 +1,8 @@
-﻿using System.Globalization;
-using System.Security.Cryptography;
-using static ObjectMetaDataTagging.ObjectTaggingExtensions;
+﻿using ObjectMetaDataTagging.Extensions;
+using ObjectMetaDataTagging.Models;
 
 namespace ObjectMetaDataTagging
 {
-    // A flexible mechanism for associating metadata with object instances.
-
-    public class Person
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-
-        public override string ToString()
-        {
-            return $"Name: {Name}, Age: {Age}";
-        }
-    }
-
-
     public class Program
     {
         static void Main(string[] args)
@@ -45,112 +30,5 @@ namespace ObjectMetaDataTagging
 
             Console.Read();
         }
-    }
-
-    public static class ObjectTaggingExtensions
-    {
-        private static Dictionary<WeakReference, List<object>> data
-           = new Dictionary<WeakReference, List<object>>();
-
-        private static readonly TaggingEventManager _eventManager = new TaggingEventManager();
-        public static event EventHandler<TagAddedEventArgs> TagAdded
-        {
-            add => _eventManager.TagAdded += value;
-            remove => _eventManager.TagAdded -= value;
-        }
-
-        public static void AddTagAddedHandler(EventHandler<TagAddedEventArgs> handler)
-        {
-            TagAdded += handler;
-        }
-
-        public static T? GetTag<T>(this object o, int tagIndex)
-        {
-            var key = data.Keys.FirstOrDefault(k => k.IsAlive && k.Target == o);
-            if (key != null && data[key].Count > tagIndex && data[key][tagIndex] is T)
-            {
-                return (T)data[key][tagIndex];
-            }
-            else
-            {
-                return default;
-            }
-        }
-
-        public static void SetTag<T>(this object o, T tag)
-        {
-            var key = data.Keys.FirstOrDefault(k => k.IsAlive && k.Target == o);
-            if (key != null)
-            {
-                data[key].Add(tag);
-            }
-            else
-            {
-                data.Add(new WeakReference(o), new List<object> { tag });
-            }
-
-            _eventManager.RaiseTagAdded(new TagAddedEventArgs(o, tag));
-        }
-
-        public static void RemoveAllTags(this object o)
-        {
-            var key = data.Keys.FirstOrDefault(k => k.IsAlive && k.Target == o);
-            if (key != null)
-            {
-                data.Remove(key);
-            }
-        }
-
-        public static IEnumerable<KeyValuePair<string, object>> GetAllTags(this object o)
-        {
-            var tags = new List<KeyValuePair<string, object>>();
-            var keys = data.Keys.Where(k => k.IsAlive && k.Target == o);
-
-            // E.g:
-            // [System.String, [System.String, foo]]
-            // [System.String, [System.Int32, 3]]
-            foreach (var key in keys)
-            {
-                var values = data[key];
-                foreach (var value in values)
-                {
-                    if (value is KeyValuePair<string, object> tag)
-                    {
-                        tags.Add(new KeyValuePair<string, object>(o.GetType().ToString(), tag));
-                    }
-                    else
-                    {
-                        tags.Add(new KeyValuePair<string, object>(o.GetType().ToString(),
-                            new KeyValuePair<string, object>(value.GetType().ToString(), value)));
-                    }
-                }
-            }
-            return tags;
-        }
-
-        public class TagAddedEventArgs : EventArgs
-        {
-            public object Object { get; set; }
-            public object Tag { get; set; }
-
-            public TagAddedEventArgs(object obj, object tag)
-            {
-                Object = obj;
-                Tag = tag;
-            }
-        }
-
-        private class TaggingEventManager
-        {
-            public event EventHandler<TagAddedEventArgs> TagAdded;
-
-            public void RaiseTagAdded(TagAddedEventArgs e)
-            {
-                TagAdded?.Invoke(this, e);
-                // ... Do some action
-                Console.WriteLine($"'{e.Tag}' tag was created and added to object: '{e.Object.GetType().Name}'.");
-            }
-        }
-    }
-
+    }  
 }
