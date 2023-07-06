983f39462556f0c92e0e64b3ad93d50896797375
Richard Smythe <richsmythe@gmail.com>
2023-07-06 12:52:59 +0100

    update

--- UI/objectrelationshipui/src/app/models/Frame.ts
+++ UI/objectrelationshipui/src/app/models/Frame.ts
@@ -5,7 +5,7 @@ export interface Frame {
   id: number;
   position: { x: number; y: number };
   size: { w: number; h: number };
-  data: string;
+
   origin: string;
   frameType: string;
   objectData?: ObjectModel[] ;
--- UI/objectrelationshipui/src/app/services/frame.service.ts
+++ UI/objectrelationshipui/src/app/services/frame.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, catchError, map, of, switchMap, tap } from 'rxjs';
 import { Frame } from '../models/Frame';
 import { ObjectModel } from '../models/ObjectModel';
+import { TagModel } from '../models/TagModel';
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +23,7 @@ export class FrameService {
           if (frameData.objectData) {
             frameData.objectData.forEach((object: ObjectModel) => {
               // Create an object frame
-              const objectFrame = this.createNewFrame(object.objectName, 'Object', frameData.id, frameData.origin);
+              const objectFrame = this.createNewFrame([object], [], 'Object', frameData.id, frameData.origin);
               frames.push(objectFrame);
   
               // Filter the tags associated with the current object
@@ -30,8 +31,7 @@ export class FrameService {
               
               // Create a tag frame and populate the grouped tags
               if (associatedTags.length > 0) {
-                const tagNames = associatedTags.map((tag: { tagName: any; }) => tag.tagName);
-                const tagFrame = this.createNewFrame(tagNames.join(', '), 'Tag', frameData.id, frameData.origin);
+                const tagFrame = this.createNewFrame([], associatedTags, 'Tag', frameData.id, frameData.origin);
                 frames.push(tagFrame);
               }
             });
@@ -48,16 +48,15 @@ export class FrameService {
     );
   }
   
-  createNewFrame(data: string, frameType: string, id: number, origin: string): Frame {
+  createNewFrame(objectData: ObjectModel[], tagData: TagModel[], frameType: string, id: number, origin: string): Frame {
     const frame: Frame = {
       id,
       position: { x: 100, y: 100 },
       size: { w: 450, h: 250 },
-      data,
-      origin,
       frameType,
-      objectData: [],
-      tagData: []
+      origin,
+      objectData,
+      tagData
     };
   
     const currentFrames = this.frames.value.slice();
@@ -66,6 +65,67 @@ export class FrameService {
   
     return frame;
   }
+
+
+
+
+
+
+
+  // getFrameData(): Observable<Frame[]> {
+  //   return this.http.get<any[]>('https://localhost:7170/api/Tag').pipe(
+  //     switchMap(response => {
+  //       const frames: Frame[] = [];
+  
+  //       response.forEach(frameData => {
+  //         // Check if there are objects
+  //         if (frameData.objectData) {
+  //           frameData.objectData.forEach((object: ObjectModel) => {
+  //             // Create an object frame
+  //             const objectFrame = this.createNewFrame(object.objectName, 'Object', frameData.id, frameData.origin);
+  //             frames.push(objectFrame);
+  
+  //             // Filter the tags associated with the current object
+  //             const associatedTags = frameData.tagData.filter((tag: { associatedObjectId: number; }) => tag.associatedObjectId === object.id);
+              
+  //             // Create a tag frame and populate the grouped tags
+  //             if (associatedTags.length > 0) {
+  //               const tagNames = associatedTags.map((tag: { tagName: any; }) => tag.tagName);
+  //               const tagFrame = this.createNewFrame(tagNames.join(', '), 'Tag', frameData.id, frameData.origin);
+  //               frames.push(tagFrame);
+  //             }
+  //           });
+  //         }
+  //       });
+  
+  //       this.frames.next(frames);
+  //       return of(frames);
+  //     }),
+  //     catchError(error => {
+  //       console.error('Error fetching frame data:', error);
+  //       return of([]);
+  //     })
+  //   );
+  // }
+  
+  // createNewFrame(data: string, frameType: string, id: number, origin: string): Frame {
+  //   const frame: Frame = {
+  //     id,
+  //     position: { x: 100, y: 100 },
+  //     size: { w: 450, h: 250 },
+  //     data,
+  //     origin,
+  //     frameType,
+  //     objectData: [],
+  //     tagData: []
+  //   };
+  
+  //   const currentFrames = this.frames.value.slice();
+  //   currentFrames.push(frame);
+  //   this.frames.next(currentFrames);
+  
+  //   return frame;
+  // }
 
   getFrameSize(frame: Frame): { width: number, height: number } | undefined {
     if (frame && frame.size) {
