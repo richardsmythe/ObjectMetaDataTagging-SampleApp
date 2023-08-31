0864a34c3521e55cc8510bd3dc65176808728c02
Richard Smythe <richsmythe@gmail.com>
2023-08-31 12:11:56 +0100

    update

---
+++ ObjectMetaDataTagging/Configuration/ServiceCollection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ObjectMetaDataTagging.Interfaces;
+
+namespace ObjectMetaDataTagging.Configuration
+{
+    /* 
+       Allows devs using my library to register all the services at once,
+       e.g. they'd use builder.Services.AddObjectMetaDataTagging();
+    */
+    public static class ServiceCollection
+    {
+        public static IServiceCollection AddObjectMetaDataTagging(this IServiceCollection services)
+        {
+            services.AddSingleton<IDefaultTaggingService, DefaultTaggingService>();
+            services.AddSingleton<ITagFactory, TagFactory>();
+
+            return services;
+        }
+    }
+}
